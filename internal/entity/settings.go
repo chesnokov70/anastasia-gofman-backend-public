d6ded162552de10f6a1394ed2c450a9ece9d7d65
sozdatel3 <110739584+sozdatel3@users.noreply.github.com>
2025-07-17 17:29:54 +0300

    FIX free settings 3

--- internal/entity/settings.go
+++ internal/entity/settings.go
@@ -6,5 +6,5 @@ import "gorm.io/datatypes"
 // There is always only one instance with ID=1.
 type Settings struct {
 	ID   uint           `json:"id" gorm:"primaryKey"`
-	Data datatypes.JSON `json:"data" gorm:"type:jsonb" swaggertype:"object" example:"{\"about_us\":\"some text\"}"`
+	Data datatypes.JSON `json:"data" gorm:"type:jsonb" swaggertype:"object"`
 }
